93e6c8b9f2308a79f84f96268a008e217bfdb996
Collin Weir <cgw21@txstate.edu>
2016-09-29 10:49:46 -0500

    These files are from the video demo

--- web_app.py
+++ web_app.py
@@ -1,11 +1,14 @@
 from flask import Flask
+import flask
 
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
-    return 'Hello World!'
+    usernames = 'bobby hill'
+    show = 'king of the hill'
+    return flask.render_template('home.html',username=usernames, show=show)
 
 
 if __name__ == '__main__':
