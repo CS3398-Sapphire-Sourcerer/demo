6ae5b7472c4ea6a01075389da67802dba7e20c7a
Collin Weir <cgw21@txstate.edu>
2016-09-29 10:53:03 -0500

    These files are from the video demo 2

--- web_app.py
+++ web_app.py
@@ -8,7 +8,8 @@ app = Flask(__name__)
 def hello_world():
     usernames = 'bobby hill'
     show = 'king of the hill'
-    return flask.render_template('home.html',username=usernames, show=show)
+    state = 'texas'
+    return flask.render_template('home.html', username=usernames, show=show, state=state)
 
 
 if __name__ == '__main__':
