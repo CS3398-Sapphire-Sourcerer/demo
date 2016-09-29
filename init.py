e823204a771a9b071777bdf43e198fba6da91f8a
collin <cgw21@txstate.edu>
2016-09-28 21:18:16 -0500

    This is an intial push to the repo for our SE project. Please pull down and add a text file.

---
+++ init.py
@@ -0,0 +1,8 @@
+import flask
+#from flask_socketio import SocketIO
+
+app = flask.Flask(__name__)
+#socketio = SocketIO(app)
+#app.config.from_pyfile('settings.py')
+
+#NOTE: We will node the socketio for async communication between the python and javascript files.
